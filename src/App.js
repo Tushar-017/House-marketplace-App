8dd32552b0197bd171bcf423b874356ab6581a4c
Tushar Rajput <tusharo17rajput@gmail.com>
2022-09-01 23:37:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import ForgotPassword from './pages/ForgotPassword'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Explore/>} />
+          <Route path='/offers' element={<Offers/>} />
+          <Route path='/profile' element={<Profile/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/forget-password' element={<ForgotPassword/>} />
+        </Routes>
+        <Navbar/>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6PEtb_-CEJ5snfXWCReMC6AbX_w8Jczk",
+  authDomain: "house-marketplace-app-413c8.firebaseapp.com",
+  projectId: "house-marketplace-app-413c8",
+  storageBucket: "house-marketplace-app-413c8.appspot.com",
+  messagingSenderId: "664666398671",
+  appId: "1:664666398671:web:39397cc6d4b5fbdc0d7cba"
+};
+
+// const app is useless here we only need to initializeApp
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore()
